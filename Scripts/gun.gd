9915bb0e800e8ecfac7ae967e3cd8acb8c74832e
Trần Quốc Nghĩa 21011618 <garenpc2003@gmail.com>
2024-04-30 12:40:30 +0700

    +[NghiaTQ] Set up shooting mechanic

--- Scripts/gun.gd
+++ Scripts/gun.gd
@@ -1,7 +1,19 @@
 extends Area2D
-
+var BULLET
+func _ready():
+	BULLET = preload("res://Scenes/Bullet.tscn")
 func _physics_process(delta):
 	var enemies = get_overlapping_bodies()
 	if enemies.size() > 0:
 		var target = enemies[0]
 		look_at(target.global_position)
+
+func Shoot():
+	var new_bullet = BULLET.instantiate()
+	new_bullet.global_position = %ShootingPoint.global_position
+	new_bullet.global_rotation = %ShootingPoint.global_rotation
+	%ShootingPoint.add_child(new_bullet)
+
+
+func _on_timer_timeout():
+	Shoot()
